75640fd4ba16b04e761d002114489a1bc97f2623
amaan-akram <amaan7akram@gmail.com>
2020-11-12 19:38:03 +0000

    idk

--- ANN.py
+++ ANN.py
@@ -1,7 +1,7 @@
 import numpy
-from random import seed
 from random import random
-import matplotlib.pyplot as plt
+import pandas
+
 
 
 # ##################### Activation Functions ##############################
@@ -25,15 +25,18 @@ def Gaussian(X):
     return numpy.exp(-numpy.square(X)/2)
 
 
-# i = input layer, h = hidden layers, o = output layers
-def new_network(inp, h, o):
+# i = input layer,
+# h = hidden layers,
+# o = output layers,
+# activationfunc = (pick one of the above),
+# data= array of inputs
+def new_network(inp, h, o, activationfunc, data):
     network = list()
     hidden_layer = [{'weights':[random() for i in range(inp + 1)]} for i in range(h)]
     network.append(hidden_layer)
     output_layer = [{'weights':[random() for i in range(h + 1)]} for i in range(o)]
     network.append(output_layer)
-    return network
-
+    return forward_propagate(network, data, activationfunc)
 
 # Calculate neuron activation for an input
 def activate(weights, inputs):
@@ -43,21 +46,28 @@ def activate(weights, inputs):
     return activation
 
 
-def forward_propagate(network, row):
+def forward_propagate(network, row, activationfunc):
     inputs = row
     for layer in network:
         new_inputs = []
         for neuron in layer:
             activation = activate(neuron['weights'], inputs)
-            neuron['output'] = sigmoid(activation)
+            neuron['output'] = activationfunc(activation)
             new_inputs.append(neuron['output'])
         inputs = new_inputs
     return inputs
 
 
-network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
-           [{'weights': [0.2550690257394217, 0.49543508709194095]},
-            {'weights': [0.4494910647887381, 0.651592972722763]}]]
-row = [1, 0, None]
-output = forward_propagate(network, row)
-print(output)
+
+
+data = [1, 1,2,1, None]
+network = new_network(1,5,2, Gaussian, data)
+print(network)
+
+
+
+#print(network)
+
+#n_inputs = len(dataset[0]) - 1
+#n_outputs = len(set([row[-1] for row in dataset]))
+#network = initialize_network(n_inputs, 2, n_outputs)
