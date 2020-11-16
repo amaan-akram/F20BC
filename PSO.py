a380b0284987df7e988b009c48e95664687d7012
amaan-akram <amaan7akram@gmail.com>
2020-11-16 00:09:50 +0000

    velocity stuff idk

--- PSO.py
+++ PSO.py
@@ -22,6 +22,15 @@ class Particle:
         print("Particle ", self.id, " at position : ", self.position, " and PB is ", self.pBest,
               " with velocity ", self.velocity)
 
+    def newVelocity(self, vel_const, g_best, i_best, p_best, i):
+
+        for j in range(i):
+            r1 = random.random()
+            r2 = random.random()
+            new_Vel = vel_const * self.velocity + p_best * r1 * (self.pBest[j] - self.position[j]) + (i_best * r2 * (self.position[j] - g_best))
+
+        return new_Vel
+
 
 def fitness(particle, inp, pred):
     x = particle.position
@@ -39,16 +48,17 @@ def fitness(particle, inp, pred):
         total += np.mean((values[i][0] - pred[i])**2)
     return total / len(values)
 
-def newVelocity():
 
 
 
 
-def PSO(swarm_size, velocity, p_best, i_best, g_best, max_iter, dimensions, bounds, file):
+
+def PSO(swarm_size, vel_const, p_best, i_best, g_best, max_iter, dimensions, bounds, file):
     inputs, exp = dp.prepare_data(file)
     g_best_value = float('inf')
     g_best_pos = np.array([float('inf'), float('inf')])
-    arr = [Particle([random.uniform(bounds[0], bounds[1]) for i in range(dimensions)], [random.uniform(0, 1),random.uniform(0,1)], i) for i in range(swarm_size)]
+    arr = [Particle([random.uniform(bounds[0], bounds[1]) for i in range(dimensions)], random.uniform(0, 1), i) for i in range(swarm_size)]
+
     iter = 0
     while iter < max_iter:
 
@@ -57,6 +67,10 @@ def PSO(swarm_size, velocity, p_best, i_best, g_best, max_iter, dimensions, boun
 
 
 
+            for i in range(len(particle.position)):
+                newVel = particle.newVelocity(vel_const=vel_const, p_best=p_best, i_best=i_best, g_best=g_best,
+                                              i=dimensions)
+                particle.position[i] = particle.position[i] + newVel
 
 
             particle_fitness = fitness(particle, inputs, exp)
@@ -69,24 +83,21 @@ def PSO(swarm_size, velocity, p_best, i_best, g_best, max_iter, dimensions, boun
                 g_best_value = particle_fitness
                 g_best_pos = particle.position
 
-            print("Particle : ", particle.pBest_value)
-
-        for particle in arr:
-            particle.position += particle.velocity
-
-
-
+            print("Particle ", particle.id," : ",particle.pBest_value)
+            print("GolbalBest: ", g_best_value)
 
         iter += 1
 
 
 
 
-network = ann.createNN(1, [3, 3, 3], 1, sigmoid)
+
+
+network = ann.createNN(1, [3, 3, 3], 1, ann.hyperbolic_Tangent)
 
 f = "Data/1in_cubic.txt"
 
-PSO(swarm_size=10, velocity=1, p_best=1, i_best=1, g_best=1, max_iter=50, dimensions=ann.dimensions_num(1, [3, 3, 3], 1), bounds=[-1, 1], file=f)
+PSO(swarm_size=10, vel_const=0.8, p_best=0.5, i_best=0.2, g_best=0.3, max_iter=100, dimensions=ann.dimensions_num(1, [3, 3, 3], 1), bounds=[-1, 1], file=f)
 
 
 
